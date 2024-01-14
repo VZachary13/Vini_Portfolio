036477b9587d62209991f8adc01eef3bc817f5ca
VZ13 <vinicius.zachary@gmail.com>
2024-01-13 18:33:49 -0700

    added form functionality via script.js. now saves to google spreadsheet and displays message

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,3 +22,16 @@ function closeMenu(){
     sideMenu.style.right = '-200px';
     menuButton.style.display = 'block';
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyAA3Z-xUvTbYNJFpSSc4xgJJkbszW7Gj3EeQ6MsesAJMpBXPc_6HxAqwSkA2Pv1waRGg/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+    var myForm = document.getElementById('form');
+    myForm.reset();
+    document.getElementById('success').style.display = 'block';
+})
